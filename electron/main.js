5d8475320d63e3807a0fecf40cf29a3b999f8308
Max <trevarcat@mail.ru>
2025-02-12 16:46:26 +0300

    Second

--- electron/main.js
+++ electron/main.js
@@ -1,21 +1,21 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-const isDev = process.env.NODE_ENV === 'development'; // Определяем режим разработки
+const isDev = process.env.NODE_ENV === 'development'; 
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1600,
+    height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'), // Если нужен preload
+      preload: path.join(__dirname, 'preload.js'), 
     },
   });
 
   if (isDev) {
-    win.loadURL('http://localhost:6969'); // Загрузка Vite-сервера
+    win.loadURL('http://localhost:6969'); 
   } else {
-    win.loadFile(path.join(app.getAppPath(), 'dist/index.html')); // Загрузка собранного приложения
+    win.loadFile(path.join(app.getAppPath(), 'dist/index.html')); 
   }
 };
 
